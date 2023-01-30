7f193de843d252cc57be704e098ce531d3525166
Edwin Contreras <edwin_david_29@live.com>
2023-01-30 17:57:09 -0500

    ios lib

---
+++ vozeroh-lib/vozeroh-lib/SWSExtension.swift
@@ -0,0 +1,14 @@
+//
+//  SWSExtension.swift
+//  vozeroh-lib
+//
+//  Created by Edwin David Contreras Torres on 30/01/23.
+//
+
+import Foundation
+
+public extension Int {
+    func rootSquare () -> Int {
+        return self*self
+    }
+}
---
+++ vozeroh-lib/vozeroh-lib/vozeroh_lib.h
@@ -0,0 +1,18 @@
+//
+//  vozeroh_lib.h
+//  vozeroh-lib
+//
+//  Created by Edwin David Contreras Torres on 30/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for vozeroh_lib.
+FOUNDATION_EXPORT double vozeroh_libVersionNumber;
+
+//! Project version string for vozeroh_lib.
+FOUNDATION_EXPORT const unsigned char vozeroh_libVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vozeroh_lib/PublicHeader.h>
+
+
